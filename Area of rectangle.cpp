e8135d55f7404b67dd762acb85f4264d5af559e0
Your Name <youremail@gmail.com>
2025-12-24 01:24:21 -0800

    Area of a rectangle

---
+++ Area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,l,w;
+	cout<<"enter the lenght\n";
+	cin>>l;
+	cout<<"enter the width\n";
+	cin>>w;
+	a=l*w;
+	cout<<"Area of rectangle\n"<<a;
+	
+}
